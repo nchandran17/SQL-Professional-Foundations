9263a7f0078de2a45b8ca77bde737507c7240fab
nchandran17 <95389125+nchandran17@users.noreply.github.com>
2026-02-04 11:28:19 -0500

    Update Machine-Processing-Time.sql
    First attempt at JOIN problem to find average time it takes machine to complete a process.

--- Advanced-Joins/Machine-Processing-Time.sql
+++ Advanced-Joins/Machine-Processing-Time.sql
@@ -1,1 +1,6 @@
-
+SELECT a.machine_id, ROUND(AVG(a1.timestamp-a.timestamp):: numeric, 3) as processing_time
+FROM Activity a
+JOIN Activity a1
+ON a.machine_id = a1.machine_id
+WHERE a.activity_type = 'start' AND a1.activity_type = 'end'
+GROUP BY a.machine_id;
