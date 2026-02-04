f0433dc8129df43e59071b06eb530b8356318389
nchandran17 <95389125+nchandran17@users.noreply.github.com>
2026-02-04 11:29:28 -0500

    Update Machine-Processing-Time.sql
    Optimization: Added process_id anchor to resolve Cartesian join explosion

--- Advanced-Joins/Machine-Processing-Time.sql
+++ Advanced-Joins/Machine-Processing-Time.sql
@@ -1,6 +1,7 @@
 SELECT a.machine_id, ROUND(AVG(a1.timestamp-a.timestamp):: numeric, 3) as processing_time
 FROM Activity a
 JOIN Activity a1
-ON a.machine_id = a1.machine_id
+ON a.machine_id = a1.machine_id 
+AND a.process_id = a1.process_id
 WHERE a.activity_type = 'start' AND a1.activity_type = 'end'
 GROUP BY a.machine_id;
